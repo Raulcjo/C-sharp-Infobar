067275e81ab4b20473549331c99a4708f38e6c26
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-10-26 21:56:20 -0300

    API - 26/10/2023

---
+++ InfoAPI/Models/PedidoInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace InfobarAPI.Models
+{
+    public class PedidoInputModel
+    {
+        //Precisa terminar 
+        public DateTime DataPedido { get; set; }
+        public string NomeProduto { get; set; }
+        public int IdColaborador { get; set; }
+        public int IdProduto { get; set; }
+
+    }
+}
