00e5b9d606dcc5f6d0ee4f1cfee8889afa9caa2f
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-11-09 14:25:32 -0300

    API - 09/11/2023

---
+++ API0911/Models/PedidoViewCol.cs
@@ -0,0 +1,9 @@
+﻿namespace InfobarAPI.Models
+{
+    public class PedidoViewCol
+    {
+        public DateTime DataPedido { get; set; }
+        public string ProdutoNome { get; set; }
+        public double Preco { get; set; }
+    }
+}
