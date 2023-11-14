3cc22305046abd3327da217ad9ff563ca355763b
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-11-13 21:56:27 -0300

    API nova versão - 13/11/2023

---
+++ API013-11/Models/ColaboradorInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace InfobarAPI.Models
+{
+    public class ColaboradorInputModel
+    {
+        public string Nome { get; set; }
+        public string Cargo { get; set; }
+        public string Credencial { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+    }
+}
---
+++ API013-11/Models/PedidoInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace InfobarAPI.Models
+{
+    public class PedidoInputModel
+    {
+        [JsonIgnore]
+        public int IdPedido { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int IdColaborador { get; set; }
+        public int IdProduto { get; set; }
+
+    }
+}
